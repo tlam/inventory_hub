eb1cacbf91276bffa6d6fada9b368f6a25e52a1d
tlam <tlam@lnx-tlam>
2010-09-11 10:34:02 -0400

    Removing old customers migration files

--- customers/migrations/0001_initial.py
+++ customers/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(SchemaMigration):
         # Adding model 'Customer'
         db.create_table('customers_customer', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('cutomer_no', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('customer_no', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('last_name', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('company_name', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
@@ -19,21 +19,21 @@ class Migration(SchemaMigration):
             ('address', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('city', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['geography.City'])),
             ('country', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['geography.Country'])),
-            ('email', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('home_phone', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('work_phone', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('cell_phone', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('email', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
             ('vat_flag', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
-            ('vat_registration_number', self.gf('django.db.models.fields.IntegerField')()),
-            ('business_registration_number', self.gf('django.db.models.fields.CharField')(max_length=9)),
-            ('discount_percent', self.gf('django.db.models.fields.FloatField')()),
+            ('vat_registration_number', self.gf('django.db.models.fields.IntegerField')(default=0, blank=True)),
+            ('business_registration_number', self.gf('django.db.models.fields.CharField')(max_length=9, blank=True)),
+            ('discount_percent', self.gf('django.db.models.fields.FloatField')(default=0, blank=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2010, 9, 11, 10, 33, 3, 325921), auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2010, 9, 11, 10, 33, 3, 325969), auto_now=True, blank=True)),
         ))
         db.send_create_signal('customers', ['Customer'])
 
-        # Adding M2M table for field phone on 'Customer'
-        db.create_table('customers_customer_phone', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('customer', models.ForeignKey(orm['customers.customer'], null=False)),
-            ('phone', models.ForeignKey(orm['communications.phone'], null=False))
-        ))
-        db.create_unique('customers_customer_phone', ['customer_id', 'phone_id'])
+        # Adding unique constraint on 'Customer', fields ['first_name', 'last_name']
+        db.create_unique('customers_customer', ['first_name', 'last_name'])
 
 
     def backwards(self, orm):
@@ -41,44 +41,42 @@ class Migration(SchemaMigration):
         # Deleting model 'Customer'
         db.delete_table('customers_customer')
 
-        # Removing M2M table for field phone on 'Customer'
-        db.delete_table('customers_customer_phone')
+        # Removing unique constraint on 'Customer', fields ['first_name', 'last_name']
+        db.delete_unique('customers_customer', ['first_name', 'last_name'])
 
 
     models = {
-        'communications.phone': {
-            'Meta': {'object_name': 'Phone'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'number': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
-            'phone_type': ('django.db.models.fields.CharField', [], {'max_length': '2'})
-        },
         'customers.customer': {
-            'Meta': {'object_name': 'Customer'},
+            'Meta': {'unique_together': "(('first_name', 'last_name'),)", 'object_name': 'Customer'},
             'address': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'business_registration_number': ('django.db.models.fields.CharField', [], {'max_length': '9'}),
+            'business_registration_number': ('django.db.models.fields.CharField', [], {'max_length': '9', 'blank': 'True'}),
+            'cell_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geography.City']"}),
             'company_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geography.Country']"}),
-            'cutomer_no': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
-            'discount_percent': ('django.db.models.fields.FloatField', [], {}),
-            'email': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 33, 3, 325921)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'customer_no': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'discount_percent': ('django.db.models.fields.FloatField', [], {'default': '0', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'home_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'phone': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['communications.Phone']", 'symmetrical': 'False'}),
             'price_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 33, 3, 325969)', 'auto_now': 'True', 'blank': 'True'}),
             'vat_flag': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'vat_registration_number': ('django.db.models.fields.IntegerField', [], {})
+            'vat_registration_number': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'work_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
         },
         'geography.city': {
             'Meta': {'object_name': 'City'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
         },
         'geography.country': {
             'Meta': {'object_name': 'Country'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
         }
     }
 
