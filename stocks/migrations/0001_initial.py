c5e1a711a2026109a34c7eb9826505e70c2661b8
tlam <tlam@lnx-tlam>
2010-09-11 10:42:27 -0400

    Regenerate stocks migration

--- stocks/migrations/0001_initial.py
+++ stocks/migrations/0001_initial.py
@@ -11,15 +11,15 @@ class Migration(SchemaMigration):
         # Adding model 'Category'
         db.create_table('stocks_category', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(default='', max_length=100)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='', unique=True, max_length=100)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
         ))
         db.send_create_signal('stocks', ['Category'])
 
         # Adding model 'Warehouse'
         db.create_table('stocks_warehouse', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(default='', max_length=100)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='', unique=True, max_length=100)),
         ))
         db.send_create_signal('stocks', ['Warehouse'])
 
@@ -27,8 +27,43 @@ class Migration(SchemaMigration):
         db.create_table('stocks_price', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('retail_price', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=20, decimal_places=2)),
+            ('measurement', self.gf('django.db.models.fields.CharField')(default='', max_length=2)),
         ))
         db.send_create_signal('stocks', ['Price'])
+
+        # Adding model 'Stock'
+        db.create_table('stocks_stock', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('item_code', self.gf('django.db.models.fields.CharField')(default='', max_length=50)),
+            ('description', self.gf('django.db.models.fields.CharField')(default='', max_length=255)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['stocks.Category'])),
+            ('retail_price', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=20, decimal_places=2)),
+            ('retail_unit', self.gf('django.db.models.fields.CharField')(default='', max_length=2, blank=True)),
+            ('wholesale_price', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=20, decimal_places=2)),
+            ('wholesale_unit', self.gf('django.db.models.fields.CharField')(default='', max_length=2, blank=True)),
+            ('dealer_price', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=20, decimal_places=2)),
+            ('dealer_unit', self.gf('django.db.models.fields.CharField')(default='', max_length=2, blank=True)),
+            ('special_price', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=20, decimal_places=2)),
+            ('special_unit', self.gf('django.db.models.fields.CharField')(default='', max_length=2, blank=True)),
+            ('pieces_per_box', self.gf('django.db.models.fields.IntegerField')(default=0, null=True, blank=True)),
+            ('exempt_flag', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('nonstock_flag', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('country', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['geography.Country'], null=True, blank=True)),
+            ('supplier', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['suppliers.Supplier'], null=True, blank=True)),
+        ))
+        db.send_create_signal('stocks', ['Stock'])
+
+        # Adding model 'StockItem'
+        db.create_table('stocks_stockitem', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('stock', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['stocks.Stock'])),
+            ('quantity', self.gf('django.db.models.fields.IntegerField')(default=0, blank=True)),
+            ('discount', self.gf('django.db.models.fields.FloatField')(default=0, blank=True)),
+            ('quotation', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['quotations.Quotation'], null=True, blank=True)),
+            ('cash_sale', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='cash_sale', null=True, to=orm['sales.CashSale'])),
+            ('credit_sale', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='credit_sale', null=True, to=orm['sales.CreditSale'])),
+        ))
+        db.send_create_signal('stocks', ['StockItem'])
 
 
     def backwards(self, orm):
@@ -42,23 +77,145 @@ class Migration(SchemaMigration):
         # Deleting model 'Price'
         db.delete_table('stocks_price')
 
+        # Deleting model 'Stock'
+        db.delete_table('stocks_stock')
+
+        # Deleting model 'StockItem'
+        db.delete_table('stocks_stockitem')
+
 
     models = {
+        'customers.customer': {
+            'Meta': {'unique_together': "(('first_name', 'last_name'),)", 'object_name': 'Customer'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'business_registration_number': ('django.db.models.fields.CharField', [], {'max_length': '9', 'blank': 'True'}),
+            'cell_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geography.City']"}),
+            'company_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geography.Country']"}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 416768)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'customer_no': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'discount_percent': ('django.db.models.fields.FloatField', [], {'default': '0', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'home_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'price_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 416810)', 'auto_now': 'True', 'blank': 'True'}),
+            'vat_flag': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'vat_registration_number': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'work_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
+        },
+        'geography.city': {
+            'Meta': {'object_name': 'City'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'geography.country': {
+            'Meta': {'object_name': 'Country'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'quotations.quotation': {
+            'Meta': {'object_name': 'Quotation'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 419257)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['customers.Customer']"}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice_no': ('django.db.models.fields.IntegerField', [], {}),
+            'remarks': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 419306)', 'auto_now': 'True', 'blank': 'True'}),
+            'warehouse': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['stocks.Warehouse']"})
+        },
+        'sales.cashsale': {
+            'Meta': {'object_name': 'CashSale'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 420356)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['customers.Customer']"}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice_no': ('django.db.models.fields.IntegerField', [], {}),
+            'recquisition_number': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'remarks': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 420403)', 'auto_now': 'True', 'blank': 'True'}),
+            'warehouse': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['stocks.Warehouse']"})
+        },
+        'sales.creditsale': {
+            'Meta': {'object_name': 'CreditSale'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 420356)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['customers.Customer']"}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice_no': ('django.db.models.fields.IntegerField', [], {}),
+            'recquisition_number': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'remarks': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 9, 11, 10, 41, 26, 420403)', 'auto_now': 'True', 'blank': 'True'}),
+            'warehouse': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['stocks.Warehouse']"})
+        },
         'stocks.category': {
             'Meta': {'object_name': 'Category'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'})
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'unique': 'True', 'max_length': '100'})
         },
         'stocks.price': {
             'Meta': {'object_name': 'Price'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'measurement': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2'}),
             'retail_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '20', 'decimal_places': '2'})
+        },
+        'stocks.stock': {
+            'Meta': {'object_name': 'Stock'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['stocks.Category']"}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geography.Country']", 'null': 'True', 'blank': 'True'}),
+            'dealer_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '20', 'decimal_places': '2'}),
+            'dealer_unit': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'exempt_flag': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item_code': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50'}),
+            'nonstock_flag': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'pieces_per_box': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
+            'retail_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '20', 'decimal_places': '2'}),
+            'retail_unit': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2', 'blank': 'True'}),
+            'special_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '20', 'decimal_places': '2'}),
+            'special_unit': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2', 'blank': 'True'}),
+            'supplier': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['suppliers.Supplier']", 'null': 'True', 'blank': 'True'}),
+            'wholesale_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '20', 'decimal_places': '2'}),
+            'wholesale_unit': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2', 'blank': 'True'})
+        },
+        'stocks.stockitem': {
+            'Meta': {'object_name': 'StockItem'},
+            'cash_sale': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'cash_sale'", 'null': 'True', 'to': "orm['sales.CashSale']"}),
+            'credit_sale': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'credit_sale'", 'null': 'True', 'to': "orm['sales.CreditSale']"}),
+            'discount': ('django.db.models.fields.FloatField', [], {'default': '0', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'quotation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['quotations.Quotation']", 'null': 'True', 'blank': 'True'}),
+            'stock': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['stocks.Stock']"})
         },
         'stocks.warehouse': {
             'Meta': {'object_name': 'Warehouse'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'})
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'unique': 'True', 'max_length': '100'})
+        },
+        'suppliers.supplier': {
+            'Meta': {'object_name': 'Supplier'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'business_registration_number': ('django.db.models.fields.CharField', [], {'max_length': '9', 'blank': 'True'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geography.City']"}),
+            'company_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geography.Country']"}),
+            'creditors_code': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'postal_code': ('django.db.models.fields.CharField', [], {'max_length': '6', 'blank': 'True'}),
+            'price_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'supplier_no': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'vat_flag': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'vat_registration_number': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'})
         }
     }
 
